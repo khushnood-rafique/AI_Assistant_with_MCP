9208fba4d9d3d0202dafea2cd78fdebb2722432a
khushnood-rafique <khushnood.rafique@cs.uni-kl.de>
2025-08-22 20:44:44 +0200

    file structure created

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='langchain',
+    version='0.1.0',
+    author='Your Name',
+    author_email='',
+    packages=find_packages(),
+    install_requires=[]
+)
